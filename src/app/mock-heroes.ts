1741b44faca8a46e2587e7d0ff0c32cc38269bb4
vibrio81 <lstoneporter@gmail.com>
2020-12-15 17:13:59 -0600

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Asta' },
+  { id: 2, name: 'Yato' },
+  { id: 3, name: 'Izuku Midoriya' },
+  { id: 4, name: 'Erin Yeager' },
+  { id: 5, name: 'Tanjiro Kamado' },
+  { id: 6, name: 'Zenitsu Agatsuma' },
+  { id: 7, name: 'Shinra Kusakabe' },
+  { id: 8, name: 'Benimaru Shinmon' },
+  { id: 9, name: 'Osamu Dazai' },
+  { id: 10, name: 'Katsuki Bakago' },
+  { id: 11, name: 'Satoru Gojo'},
+];
